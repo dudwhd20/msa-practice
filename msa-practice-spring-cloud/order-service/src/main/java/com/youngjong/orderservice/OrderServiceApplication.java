ae6eb4d6e0e0eaa47f00db4ee9e9f9827dbc42af
soyj <dudwhd20@naver.com>
2025-06-11 10:08:44 +0900

    chore: update feign setting

--- msa-practice-spring-cloud/order-service/src/main/java/com/youngjong/orderservice/OrderServiceApplication.java
+++ msa-practice-spring-cloud/order-service/src/main/java/com/youngjong/orderservice/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.youngjong.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.youngjong.orderservice.infrastructure")
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
