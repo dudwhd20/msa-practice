65cf37bcfe12afb26c916b382c46c527455b5726
soyj <dudwhd20@naver.com>
2025-07-28 16:33:27 +0900

    chore: update feignClient setting

--- msa-practice-k8s/order-service/src/main/java/com/youngjong/orderservice/infrastructure/ProductClient.java
+++ msa-practice-k8s/order-service/src/main/java/com/youngjong/orderservice/infrastructure/ProductClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "product-service", configuration = FeignOkHttpConfig.class)
+@FeignClient(name = "product-service", configuration = FeignOkHttpConfig.class, url = "${product.service.url}")
 public interface ProductClient {
 
     @GetMapping("/products/{id}")
